38b17f38e8e68ca3ed21cb323e2b96dfa47c3d7f
GleVol17 <70822830+GleVol17@users.noreply.github.com>
2025-10-31 12:17:22 +0200

    added async and semaphoreSlim for repository

--- LibraryManagement/LibraryManagement/Repositories/IBookRepository.cs
+++ LibraryManagement/LibraryManagement/Repositories/IBookRepository.cs
@@ -6,10 +6,10 @@ public interface IBookRepository
 {
     IEnumerable<Book> GetAll();
     Book? GetByCode(int code);
-    void Remove(int code);
+    Task RemoveAsync(int code);
     IEnumerable<Book> Search(string query);
-    void Update(Book book);
+    Task UpdateAsync(Book book);
     int GetNextCode();
-    void Persist();
-    void Add(Book book);
+    Task PersistAsync();
+    Task AddAsync(Book book);
 }
