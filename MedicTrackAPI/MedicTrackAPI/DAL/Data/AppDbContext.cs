19b2dd2c0adead5f029fe1922fcbf31f1f7c2749
GleVol17 <70822830+GleVol17@users.noreply.github.com>
2025-11-06 18:24:05 +0200

    created DbContext and added controllers and models for identity and register

---
+++ MedicTrackAPI/MedicTrackAPI/DAL/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using MedicTrackAPI.DAL.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicTrackAPI.DAL.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
