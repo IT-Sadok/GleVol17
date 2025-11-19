fc1cfaddf3927e7bfbac0e95c09356b34b63aa87
GleVol17 <70822830+GleVol17@users.noreply.github.com>
2025-11-19 18:09:18 +0200

    added dbContext and extensions

---
+++ MedicTrackAPI/MedicTrack.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,7 @@
+﻿using MedicTrack.Infrastructure.IdentityModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicTrack.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<AppUser>(options);
---
+++ MedicTrackAPI/MedicTrack.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using MedicTrack.Infrastructure.Data;
+using MedicTrack.Infrastructure.IdentityModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MedicTrack.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // DbContext
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection")));
+
+        // Identity (аналог твого коду з Program.cs)
+        services
+            .AddIdentityCore<AppUser>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            })
+            .AddRoles<IdentityRole>()
+            .AddSignInManager<SignInManager<AppUser>>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+        return services;
+    }
+}
