32eb1baba0a5553fa97e1da5e34fdfd1094ea70b
GleVol17 <70822830+GleVol17@users.noreply.github.com>
2025-11-19 18:08:18 +0200

    added nugets and migration

---
+++ MedicTrackAPI/MedicTrackAPI/AuthControllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using MedicTrack.Infrastructure.IdentityModels;
+using MedicTrack.Infrastructure.Services;
+using MedicTrackAPI.AuthModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicTrackAPI.AuthControllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController(
+    UserManager<AppUser> userManager,
+    SignInManager<AppUser> signInManager,
+    IJwtService jwtService)
+    : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var user = new AppUser
+        {
+            UserName = model.Email,
+            Email = model.Email,
+            FirstName = model.FirstName,
+            LastName = model.LastName,
+            BirthDate = model.BirthDate
+        };
+
+        var result = await userManager.CreateAsync(user, model.Password);
+
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return Ok("User registered successfully");
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var user = await userManager.FindByEmailAsync(model.Email);
+        if (user == null)
+        {
+            return Unauthorized("Invalid credentials");
+        }
+
+        var result = await signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+        if (!result.Succeeded)
+        {
+            return Unauthorized("Invalid credentials");
+        }
+
+        var token = jwtService.GenerateToken(user);
+        return Ok(new
+        {
+            token
+        });
+    }
+}
