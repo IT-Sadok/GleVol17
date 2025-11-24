46e3b364f7b3253b5ac83ed286d69e835d4ca02c
GleVol17 <70822830+GleVol17@users.noreply.github.com>
2025-11-24 13:45:12 +0200

    added IAuthenticationService in application layer

---
+++ MedicTrackAPI/MedicTrack.Application/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MedicTrack.Application.Interfaces;
+
+public interface IAuthenticationService
+{
+    Task<IdentityResult> RegisterAsync(
+        string email,
+        string password,
+        string firstName,
+        string lastName,
+        DateTime birthDate);
+
+    Task<string?> LoginAsync(string email, string password);
+}
