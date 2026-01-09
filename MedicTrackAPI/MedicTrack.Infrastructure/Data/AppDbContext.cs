5cb465092c57fcedf15c0299ffab154a376795fd
GleVol17 <70822830+GleVol17@users.noreply.github.com>
2026-01-09 18:17:25 +0200

    fixed BirthDate column type, used date

--- MedicTrackAPI/MedicTrack.Infrastructure/Data/AppDbContext.cs
+++ MedicTrackAPI/MedicTrack.Infrastructure/Data/AppDbContext.cs
@@ -4,4 +4,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MedicTrack.Infrastructure.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<AppUser>(options);
+public class AppDbContext(DbContextOptions<AppDbContext> options)
+    : IdentityDbContext<AppUser>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<AppUser>()
+            .Property(x => x.BirthDate)
+            .HasColumnType("date");
+    }
+}
---
+++ MedicTrackAPI/MedicTrack.Infrastructure/Migrations/20260109150112_FixBirthDateType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicTrack.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixBirthDateType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+    }
+}
