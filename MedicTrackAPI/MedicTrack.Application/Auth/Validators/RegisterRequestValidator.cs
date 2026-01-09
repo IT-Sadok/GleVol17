7cc79558882100daa22863946dea636d3a3f5945
GleVol17 <70822830+GleVol17@users.noreply.github.com>
2026-01-09 18:16:40 +0200

    added FluentValidation validators for auth requests

---
+++ MedicTrackAPI/MedicTrack.Application/Auth/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MedicTrack.Application.Auth.Requests;
+
+namespace MedicTrack.Application.Auth.Validators;
+
+public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+{
+    public RegisterRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(6);
+
+        RuleFor(x => x.FirstName)
+            .NotEmpty();
+
+        RuleFor(x => x.LastName)
+            .NotEmpty();
+
+        RuleFor(x => x.BirthDate)
+            .LessThan(DateTime.UtcNow.Date);
+    }
+}
