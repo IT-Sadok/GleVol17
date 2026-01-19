5f6efb6c0246999d6bddc76126f78b6cb525de48
GleVol17 <70822830+GleVol17@users.noreply.github.com>
2026-01-19 16:14:00 +0200

    removed DataAnnotations from API models

--- MedicTrackAPI/MedicTrackAPI/AuthModels/LoginModel.cs
+++ MedicTrackAPI/MedicTrackAPI/AuthModels/LoginModel.cs
@@ -4,8 +4,6 @@ namespace MedicTrackAPI.AuthModels;
 
 public class LoginModel
 {
-    [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
-    [Required, MinLength(6)]
     public string Password { get; set; } = string.Empty;
 }
--- MedicTrackAPI/MedicTrackAPI/AuthModels/RegisterModel.cs
+++ MedicTrackAPI/MedicTrackAPI/AuthModels/RegisterModel.cs
@@ -4,13 +4,9 @@ namespace MedicTrackAPI.AuthModels;
 
 public class RegisterModel
 {
-    [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
-    [Required, MinLength(6)]
     public string Password { get; set; } = string.Empty;
-    [Required]
     public string FirstName { get; set; } = string.Empty;
-    [Required]
     public string LastName { get; set; } = string.Empty;
     public DateTime BirthDate { get; set; }
 }
